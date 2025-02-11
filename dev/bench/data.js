b0ecd46252d9f93033fb5ed5fd583ed4bd6f9509
github-action-benchmark <github@users.noreply.github.com>
2025-02-11 12:37:26 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 411c71b19816006b8195250f51921cc72cfcfa7b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739276789576,
+  "lastUpdate": 1739277446505,
   "repoUrl": "https://github.com/mlefkir/benchspec",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00002659413873452125",
             "extra": "mean: 617.1504742267836 usec\nrounds: 873"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mehdylefkir.work@gmail.com",
+            "name": "Mehdy Lefkir",
+            "username": "mlefkir"
+          },
+          "committer": {
+            "email": "mehdylefkir.work@gmail.com",
+            "name": "Mehdy Lefkir",
+            "username": "mlefkir"
+          },
+          "distinct": true,
+          "id": "411c71b19816006b8195250f51921cc72cfcfa7b",
+          "message": "add empty directory",
+          "timestamp": "2025-02-11T12:36:51Z",
+          "tree_id": "188461c8b5ebc4dc21791915aa3ae8f108841894",
+          "url": "https://github.com/mlefkir/benchspec/commit/411c71b19816006b8195250f51921cc72cfcfa7b"
+        },
+        "date": 1739277446217,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test/test_jaxspec.py::test_jaxspec",
+            "value": 1437.3485600730812,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000100515568819695",
+            "extra": "mean: 695.7254682532645 usec\nrounds: 756"
           }
         ]
       }
